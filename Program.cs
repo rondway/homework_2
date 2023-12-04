35e4d1f22038366c4de3292f9c9b2028e0b726d0
Roman <romanivanov_2019@inbox.ru>
2023-12-04 12:38:17 +0300

    ыфывыфв

--- Program.cs
+++ Program.cs
@@ -1,126 +1,41 @@
-﻿//task1
-using System;
+﻿using System;
 
 class Program
 {
     static void Main()
     {
-        
-        Random random = new Random();
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        Console.Write("Введите номер строки: ");
+        int row = int.Parse(Console.ReadLine());
 
-       
-        int[] array = new int[10];
-        for (int i = 0; i < array.Length; i++)
+        Console.Write("Введите номер столбца: ");
+        int col = int.Parse(Console.ReadLine());
+
+        int result = GetElementValue(matrix, row, col);
+
+        if (result != int.MinValue)
         {
-            array[i] = random.Next(1, 101);
+            Console.WriteLine($"Значение элемента в позиции ({row}, {col}): {result}");
         }
-
-       
-        Console.WriteLine("Случайно сгенерированный массив:");
-        foreach (int element in array)
+        else
         {
-            Console.Write(element + " ");
+            Console.WriteLine($"Элемента в позиции ({row}, {col}) не существует.");
         }
-        Console.WriteLine();
+    }
 
-        int count = 0;
-        foreach (int element in array)
+    static int GetElementValue(int[,] matrix, int row, int col)
+    {
+        if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
         {
-            if (element >= 20 && element <= 90)
-            {
-                count++;
-            }
+            return matrix[row, col];
         }
-
-        Console.WriteLine($"Количество элементов в диапазоне [20, 90]: {count}");
+        else
+        {
+            return int.MinValue;
+        }
     }
 }
-
-//task 2
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-       
-//         Random random = new Random();
-
-//         int[] array = new int[10];
-//         for (int i = 0; i < array.Length; i++)
-//         {
-//             array[i] = random.Next(1, 101);
-//         }
-
-//         Console.WriteLine("Массив:");
-//         foreach (int element in array)
-//         {
-//             Console.Write(element + " ");
-//         }
-//         Console.WriteLine();
-
-       
-//         int evenCount = 0;
-//         foreach (int element in array)
-//         {
-//             if (element % 2 == 0)
-//             {
-//                 evenCount++;
-//             }
-//         }
-
-//         Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
-//     }
-// }
-
-//task 3
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-        
-//         Random random = new Random();
-
-        
-//         double[] array = new double[10];
-//         for (int i = 0; i < array.Length; i++)
-//         {
-//             array[i] = Math.Round(random.NextDouble() * 100, 2); 
-//         }
-
-       
-//         Console.WriteLine("Массив:");
-//         foreach (double element in array)
-//         {
-//             Console.Write(element + " ");
-//         }
-//         Console.WriteLine();
-
-        
-//         double max = array[0];
-//         double min = array[0];
-
-//         foreach (double element in array)
-//         {
-//             if (element > max)
-//             {
-//                 max = element;
-//             }
-
-//             if (element < min)
-//             {
-//                 min = element;
-//             }
-//         }
-
-        
-//         double difference = max - min;
-
-       
-//         Console.WriteLine($"Максимальный элемент: {max}");
-//         Console.WriteLine($"Минимальный элемент: {min}");
-//         Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
-//     }
-// }
