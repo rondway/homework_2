91db22ec0f61bc7a3047cfeaa0752a30bc9421a1
Roman <romanivanov_2019@inbox.ru>
2023-12-04 12:45:29 +0300

    ждывапопа

--- Program.cs
+++ Program.cs
@@ -1,45 +1,45 @@
 ﻿//первая задача//
-// using System;
+using System;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         int[,] matrix = {
-//             {1, 2, 3},
-//             {4, 5, 6},
-//             {7, 8, 9}
-//         };
-//         Console.Write("Введите номер строки: ");
-//         int row = int.Parse(Console.ReadLine());
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        Console.Write("Введите номер строки: ");
+        int row = int.Parse(Console.ReadLine());
 
-//         Console.Write("Введите номер столбца: ");
-//         int col = int.Parse(Console.ReadLine());
+        Console.Write("Введите номер столбца: ");
+        int col = int.Parse(Console.ReadLine());
 
-//         int result = GetElementValue(matrix, row, col);
+        int result = GetElementValue(matrix, row, col);
 
-//         if (result != int.MinValue)
-//         {
-//             Console.WriteLine($"Значение элемента в позиции ({row}, {col}): {result}");
-//         }
-//         else
-//         {
-//             Console.WriteLine($"Элемента в позиции ({row}, {col}) не существует.");
-//         }
-//     }
+        if (result != int.MinValue)
+        {
+            Console.WriteLine($"Значение элемента в позиции ({row}, {col}): {result}");
+        }
+        else
+        {
+            Console.WriteLine($"Элемента в позиции ({row}, {col}) не существует.");
+        }
+    }
 
-//     static int GetElementValue(int[,] matrix, int row, int col)
-//     {
-//         if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
-//         {
-//             return matrix[row, col];
-//         }
-//         else
-//         {
-//             return int.MinValue;
-//         }
-//     }
-// }
+    static int GetElementValue(int[,] matrix, int row, int col)
+    {
+        if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+        {
+            return matrix[row, col];
+        }
+        else
+        {
+            return int.MinValue;
+        }
+    }
+}
 
 //2 задача//
 // using System;
@@ -95,70 +95,70 @@
 // }
 
 //задача 3//
-using System;
+// using System;
 
-class Program
-{
-    static void Main()
-    {
-        int[,] matrix = GenerateRandomMatrix(3, 4);
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[,] matrix = GenerateRandomMatrix(3, 4);
 
-        Console.WriteLine("Исходный массив:");
-        PrintMatrix(matrix);
+//         Console.WriteLine("Исходный массив:");
+//         PrintMatrix(matrix);
 
-        int minRow = FindRowWithMinSum(matrix);
+//         int minRow = FindRowWithMinSum(matrix);
 
-        Console.WriteLine($"\nСтрока с наименьшей суммой элементов: {minRow + 1}");
-    }
+//         Console.WriteLine($"\nСтрока с наименьшей суммой элементов: {minRow + 1}");
+//     }
 
-    static int[,] GenerateRandomMatrix(int rows, int cols)
-    {
-        Random random = new Random();
-        int[,] matrix = new int[rows, cols];
+//     static int[,] GenerateRandomMatrix(int rows, int cols)
+//     {
+//         Random random = new Random();
+//         int[,] matrix = new int[rows, cols];
 
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < cols; col++)
-            {
-                matrix[row, col] = random.Next(1, 101); 
-            }
-        }
+//         for (int row = 0; row < rows; row++)
+//         {
+//             for (int col = 0; col < cols; col++)
+//             {
+//                 matrix[row, col] = random.Next(1, 101); 
+//             }
+//         }
 
-        return matrix;
-    }
+//         return matrix;
+//     }
 
-    static int FindRowWithMinSum(int[,] matrix)
-    {
-        int minSum = int.MaxValue;
-        int minRow = -1;
+//     static int FindRowWithMinSum(int[,] matrix)
+//     {
+//         int minSum = int.MaxValue;
+//         int minRow = -1;
 
-        for (int row = 0; row < matrix.GetLength(0); row++)
-        {
-            int currentSum = 0;
-            for (int col = 0; col < matrix.GetLength(1); col++)
-            {
-                currentSum += matrix[row, col];
-            }
+//         for (int row = 0; row < matrix.GetLength(0); row++)
+//         {
+//             int currentSum = 0;
+//             for (int col = 0; col < matrix.GetLength(1); col++)
+//             {
+//                 currentSum += matrix[row, col];
+//             }
 
-            if (currentSum < minSum)
-            {
-                minSum = currentSum;
-                minRow = row;
-            }
-        }
+//             if (currentSum < minSum)
+//             {
+//                 minSum = currentSum;
+//                 minRow = row;
+//             }
+//         }
 
-        return minRow;
-    }
+//         return minRow;
+//     }
 
-    static void PrintMatrix(int[,] matrix)
-    {
-        for (int row = 0; row < matrix.GetLength(0); row++)
-        {
-            for (int col = 0; col < matrix.GetLength(1); col++)
-            {
-                Console.Write(matrix[row, col] + " ");
-            }
-            Console.WriteLine();
-        }
-    }
-}
+//     static void PrintMatrix(int[,] matrix)
+//     {
+//         for (int row = 0; row < matrix.GetLength(0); row++)
+//         {
+//             for (int col = 0; col < matrix.GetLength(1); col++)
+//             {
+//                 Console.Write(matrix[row, col] + " ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
